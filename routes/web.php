5a42a59a7982bbb506cdbc2dfa5aff16deecafab
3B032062 <3B032062@gm.student.ncut.edu.tw>
2024-01-06 00:08:21 +0800

    新增與Product相關的CRUD路由

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,20 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\ProductController;
-Route::resource('products',ProductController::class)->only([
-
-]);
+Route::resource('products',ProductController::class);
+/*
+ * product.index:GET '/' ProductController@index , 列出所有產品
+ *
+ * product.show:GET|HEAD 'products/{product}' , ProductController@show , 檢視某一產品
+ *
+ * product.create:GET 'products' , ProductController@create , 產生產品新增的頁面
+ *
+ * product.store:POST 'products' , ProductController@store , 儲存新增的產品
+ *
+ * product.edit:GET|HEAD 'products/{product}/edit' , ProductController@update , 產生某一產品的修改頁面
+ *
+ * product.update:PUT|PATCH 'products/{product}' , ProductController@update , 更新某一產品
+ *
+ * product.destroy:DELETE 'products/{product}' , ProductController@destroy , 刪除某一產品
+ * 
+ */
