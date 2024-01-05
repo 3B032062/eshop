3b3f8b7cbe019b64a6c251ea07bd9559f5a6fe60
3B032062 <3B032062@gm.student.ncut.edu.tw>
2024-01-06 00:20:09 +0800

    建立與各個Model之間的關係

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
+
