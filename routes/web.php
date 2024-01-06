da9e9d7959d29d3ffd6c08c2a30cc78470b35d44
3B032062 <3B032062@gm.student.ncut.edu.tw>
2024-01-07 03:48:01 +0800

    發現breeze沒有require進專案 重新require一次

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\CartItemController;
-use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,43 +13,10 @@ use App\Http\Controllers\CategoryProductController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-use App\Models\User;
+
 Route::get('/', function () {
-    //return view('welcome');
-    $user = new user();
-    $user ->name = 'aa';
-    $user ->email ='12223';
-
+    return view('welcome');
 });
-
-
-Route::resource('products',ProductController::class);
-/*
- * product.index:GET '/' ProductController@index , 列出所有產品
- *
- * product.show:GET|HEAD 'products/{product}' , ProductController@show , 檢視某一產品
- *
- * product.create:GET 'products' , ProductController@create , 產生產品新增的頁面
- *
- * product.store:POST 'products' , ProductController@store , 儲存新增的產品
- *
- * product.edit:GET|HEAD 'products/{product}/edit' , ProductController@update , 產生某一產品的修改頁面
- *
- * product.update:PUT|PATCH 'products/{product}' , ProductController@update , 更新某一產品
- *
- * product.destroy:DELETE 'products/{product}' , ProductController@destroy , 刪除某一產品
- *
- */
-
-Route::resource('categories.products', CategoryProductController::class)->only([
-    'index',
-]);
-
-Route::resource('cart_items', CartItemController::class);
-Route::resource('orders', OrderController::class);
-
-
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -64,4 +28,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
+require __DIR__.'/auth.php';
